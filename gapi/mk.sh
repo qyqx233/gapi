8c6299015285663c83564008edac77370178811c
zhou.han <zhou.han@yuancore.com>
2023-03-01 16:49:34 +0800

    优化编译脚本

--- gapi/mk.sh
+++ gapi/mk.sh
@@ -1,10 +1,9 @@
-
-go build -ldflags="-s -w" *.go -o gapi
+go build -ldflags="-s -w"  -o gapi
 
 if command -v upx > /dev/null 2>&1; then
   WITH_UPX=1
 else
-  WITH_UPX=1
+  WITH_UPX=0
 fi
 
 if [ $WITH_UPX -eq 1 ];then
